0b1e954cd02ff60b3459d4b0dbcf2c25261a78c2
Cristian Vega <crisda.arive.1503@gmail.com>
2024-01-18 02:51:22 -0500

    First commit

---
+++ client/next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdnb.artstation.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdna.artstation.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ client/tailwind.config.ts
@@ -0,0 +1,60 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#101014",
+        foreground: "#ffff",
+        input: {
+          DEFAULT: "#101014",
+          border: "#505054",
+        },
+        thumbnailContainer: "#000000",
+
+        secondary: {
+          DEFAULT: "",
+          foreground: "#aaaaae",
+        },
+        muted: {
+          DEFAULT: "#404044",
+          foreground: "#e6e6ea",
+          hover: "#505054",
+        },
+
+        accent: {
+          DEFAULT: "#26bbff",
+          foreground: "#101014",
+          hover: "#74dafde8",
+        },
+
+        aside: {
+          DEFAULT: "#202024",
+        },
+
+        tag: {
+          DEFAULT: "#404044",
+          foreground: "#e6e6ea",
+        },
+
+        danger: {
+          DEFAULT: "hsl( 0 62.8% 30.6%)",
+          foreground: "hsl( 0 0% 98%)",
+        },
+
+        modal: {
+          DEFAULT: "#101014",
+          buttonCancel: "hsl( 0 62.8% 30.6%)",
+          buttonConfirm: "#404044",
+        },
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animated")],
+}
+export default config
